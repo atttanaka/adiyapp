992f904f59a51c182f7c91f64f5812381669ec17
atttanaka <dheshiko@gmail.com>
2017-02-27 15:42:14 +0900

    add devise-admin model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  devise_for :admins
   devise_for :users
   root :to => 'pages#index'
 
+  resource :adminpage
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
